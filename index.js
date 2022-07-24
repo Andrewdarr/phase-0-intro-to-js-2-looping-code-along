62aa8326f4fb4f07eee9d0d56cbcc80dd21b0a67
Andrew Darr <kildar07@gmail.com>
2022-07-24 12:43:56 -0500

    commit tree 8b629ce149a751176d0284dd2e3716e71cdb03c1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(cards) {
+  let i = 0; 
+  const name = [1, 2, 3];
+      console.log ("Thank you [1]");
+  debugger
+    }
+
+
+let countDown = (10);
+  while (countDown >= 0) {
+  console.log(countDown--);
+}
+
